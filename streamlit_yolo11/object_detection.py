c1d8880c076d1e5369870cba9b158f135dd98f51
351511 <CCCxingy351511@outlook.com>
2026-06-02 21:15:32 +0800

    删除重复冗余文件夹streamlit_yolo11

--- streamlit_yolo11/object_detection.py
+++
@@ -1,9 +0,0 @@
-#0bject Detection on Image
-# #Import All the Required Libraries
-from ultralytics import YOLO
-
-#Load the YOL011 Pre-Trained Model
-model = YOLO("yolo11n.pt")
-#Perform Object Detection on an Image
-results = model.predict("resources/images/baggage_claim.PNG", save=True)
-
